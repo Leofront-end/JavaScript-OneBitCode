2b53cde4d32629a37acf94a4d6664184f11c6fe0
Leofront-end <ls171258@gmail.com>
2024-04-24 21:15:36 -0300

    aula 2

---
+++ aula 2 - Tipos de Dados/TiposDeDados.js
@@ -0,0 +1,17 @@
+//String
+console.log('Esse é um "texto" com aspas')
+console.log(`Olá, mundo`)
+console.log('Leo'[2])
+console.log('Leo'[1])
+//Boolean
+console.log(true)
+console.log(false)
+//Number
+console.log(42)
+console.log("42")
+console.log("true")
+console.log(8.5)
+console.log(Number.MAX_SAFE_INTEGER)
+//BigInt
+console.log(42n + 5n)
+console.log(9999999999999999n * 44444444444444444n)
---
+++ aula 2 - Tipos de Dados/variavies.js
@@ -0,0 +1,15 @@
+var texto = null
+var numero = 99
+console.log(texto)
+console.log(numero)
+
+
+/*
+Const é fixo
+let é deixe mudar
+*/
+let nome = "Isaac"
+const num = 42
+nome = "Lucas"
+console.log(nome)
+console.log(num)
