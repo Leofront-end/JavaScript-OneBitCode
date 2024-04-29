3c58167d5fb6daa179e89b9494c53290ddc5ca8a
Leofront-end <ls171258@gmail.com>
2024-04-29 13:38:34 -0300

    aula 2

---
+++ aula 2 - Tipos de Dados/OperaçõesMatematicas.js
@@ -0,0 +1,13 @@
+let n = 42
+
+n -= 5
+
+console.log(n)
+
+
+
+const nome = prompt("Qual é o seu nome?")
+let idade = prompt("Quantos anos você tem?")
+idade = parseInt(idade)
+
+alert(`Seu nome é ${nome} e ano que vem você terá ${idade + 1} anos` )
