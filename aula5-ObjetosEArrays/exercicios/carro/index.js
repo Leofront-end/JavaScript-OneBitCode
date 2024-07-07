a48c6eb1d808e10235182add4bc36be4cc5c425b
Leofront-end <ls171258@gmail.com>
2024-07-07 16:02:09 -0300

    ex Array

---
+++ aula5-ObjetosEArrays/exercicios/carro/index.js
@@ -0,0 +1,8 @@
+var veiculo = {}
+
+veiculo.modelo = prompt("Digite o modelo do Carro:")
+veiculo.passageiros = parseInt(prompt("Digite o numero de passageiros:"))
+veiculo.limiteDepassageiros = parseInt(prompt("Digite o limite de passageiros:"))
+
+console.log(veiculo)
+console.log(veiculo.modelo)
