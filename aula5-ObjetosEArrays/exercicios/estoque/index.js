60d2640b1a75b9ec754b30e66c576cd83f913ad0
Leo Front-end <ls171258@gmail.com>
2024-07-10 09:30:05 -0300

    ex 003 array

--- aula5-ObjetosEArrays/exercicios/estoque/index.js
+++ aula5-ObjetosEArrays/exercicios/estoque/index.js
@@ -1,22 +1,47 @@
 let estoque = []
 
-function Adicionar() {
-    let nome = prompt("Nome do produto")
-    let quantidade = parseInt(prompt("Quantidade:"))
-    estoque.push({nome , quantidade})
+function Adicionar(){
+    estoque.push({nome:prompt("Digite o nome:"), quantidade:prompt("Digite a quantidade:")})
 }
 
-function Listar(){
-    estoque.forEach( function (elementoAtual, indice, ArrayCompleto) {
-        alert(`${indice} é ${estoque[indice]}`)
+function Listar() {
+    estoque.forEach(function (estoquinho, indice){
+        alert(`${indice} - ${estoquinho.nome} e ${estoquinho.quantidade}`)
     })
 }
 
+function Alterar() {
+    let Pesquisar = prompt("O nome que deseja procurar:")
+    estoque.find(function(estocando) {
+        if (estocando.nome == Pesquisar){
+            estocando.quantidade = prompt("Digite a nova quantidade")
+            return estocando.quantidade
+        } else {
+            alert("Nome não encontrado, tente novamente")
+        }
+    })
+}
+
+function Remover(){
+    let Pesquisa = prompt("Digite o nome que está procurando: ")
+    
+    if (estoque.nome == Pesquisa){
+        console.log(estoque.indexOf())
+    }
+
+}
+
 do {
-    var opcao = prompt("1 - Adicionar\n2 - Listar\n0 - Sair")
+    var opcao = parseFloat(prompt('1 - Adiciona\n2 - Lista\n3 - Alterar\n4 - Remover\n0 - Sair'))
     if (opcao == 1 ){
         Adicionar()
     } else if (opcao == 2){
         Listar()
+    } else if (opcao == 3){
+        Alterar()
+    } else if (opcao == 4){
+        Remover()
+    } else if(opcao != 0) {
+        alert("Número Invalido")
     }
 } while (opcao != 0);
