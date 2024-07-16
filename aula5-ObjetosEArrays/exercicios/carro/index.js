d2a3a04303feca64ca71d867a9c88ba3de84ca4b
Leo Front-end <ls171258@gmail.com>
2024-07-16 16:11:38 -0300

    ex2 Array correção

--- aula5-ObjetosEArrays/exercicios/carro/index.js
+++ aula5-ObjetosEArrays/exercicios/carro/index.js
@@ -1,12 +1,19 @@
 var veiculo = {}
 
-veiculo.modelo = prompt("Digite o modelo do Carro:")
-veiculo.passageiros = parseInt(prompt("Digite o numero de passageiros:"))
-veiculo.limiteDepassageiros = parseInt(prompt("Digite o limite de passageiros:"))
-veiculo.velocidade = parseFloat(prompt("Digite a velocidade (em Km/h):"))
+while (true) {
+    veiculo.modelo = prompt("Digite o modelo do Carro:")
+    veiculo.passageiros = parseInt(prompt("Digite o numero de passageiros:"))
+    veiculo.limiteDepassageiros = parseInt(prompt("Digite o limite de passageiros:"))
+    if (veiculo.passageiros > veiculo.limiteDepassageiros){
+        alert("Altere o numero de limite de passageiros ou de passageiros")
+        
+    }
+    veiculo.velocidade = parseFloat(prompt("Digite a velocidade (em Km/h):"))
+    if(veiculo.modelo.length !=0 && veiculo.passageiros.length !=0 && veiculo.limiteDepassageiros.length !=0 && veiculo.velocidade.length !=0 && veiculo.passageiros < veiculo.limiteDepassageiros){
+        break
+    }
+}
 
-console.log(veiculo)
-console.log(veiculo.velocidade)
 
 // Acelerar
 function Aumentar(){
