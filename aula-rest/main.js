77afe9c5f800836ec3c3ce0bd081782721f1602d
Leo Front-end <ls171258@gmail.com>
2024-08-29 18:41:17 -0300

    aula 8

--- aula-rest/main.js
+++ aula-rest/main.js
@@ -3,19 +3,35 @@ const titleInput = document.getElementById("title")
 const bodyInput = document.getElementById("body")
 const postsList = document.getElementById("postsList")
 
-postForm.addEventListener("submit",function (evento){
-  evento.preventDefault()
+carregarPosts()
+
+postForm.addEventListener("submit", function(event){
+  event.preventDefault()
 
   const title = titleInput.value
   const body = bodyInput.value
 
   criarPost(title, body)
+
+  titleInput.value = ""
+  bodyInput.value = ""
 })
 
-async function criarPost(title, body) {
- const resposta = await fetch("http://localhost:3000/posts", {
-  method: "POST",
-  body: `{"title":${title},"body":${body}}`
- })
- console.log(resposta)
+async function criarPost(title, body){
+  const resposta = await fetch("http://localhost:3000/posts", {
+    method: "POST",
+    headers: {
+      'Content-Type': "application/json"
+    },
+    body: JSON.stringify({ title, body})
+  })
+
+  console.log(resposta)
+}
+
+async function carregarPosts() {
+  const resposta = await fetch("http://localhost:3000/posts")
+  const posts = await resposta.json()
+
+  console.log(posts)
 }
