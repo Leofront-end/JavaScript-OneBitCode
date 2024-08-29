27c4c9c7294b515c0e74e0394666b7795648289e
Leo Front-end <ls171258@gmail.com>
2024-08-29 13:47:38 -0300

    aula 8

--- aula-rest/main.js
+++ aula-rest/main.js
@@ -1,6 +1,21 @@
-async function Executar() {
-  const dados = await fetch("http://localhost:3000/tarefas").then(respota => respota.json())
-  console.log(dados)
-}
+const postForm = document.getElementById("postForm")
+const titleInput = document.getElementById("title")
+const bodyInput = document.getElementById("body")
+const postsList = document.getElementById("postsList")
 
-Executar()
+postForm.addEventListener("submit",function (evento){
+  evento.preventDefault()
+
+  const title = titleInput.value
+  const body = bodyInput.value
+
+  criarPost(title, body)
+})
+
+async function criarPost(title, body) {
+ const resposta = await fetch("http://localhost:3000/posts", {
+  method: "POST",
+  body: `{"title":${title},"body":${body}}`
+ })
+ console.log(resposta)
+}
