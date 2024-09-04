0df4d482f19d8c3d677ed9410c974f7959a65aea
Leo Front-end <ls171258@gmail.com>
2024-09-04 14:41:18 -0300

    exercicio 1 aula 8

--- ex1Aula8/index.js
+++ ex1Aula8/index.js
@@ -7,6 +7,8 @@ async function executando() {
     dados.forEach(function (users){
         const userElement = document.createElement("article")
 
+        const userUsuario = document.createElement("article")
+
         const userName = document.createElement("h2")
         userName.textContent = users.name
 
@@ -23,10 +25,48 @@ async function executando() {
         userWebsite.textContent = users.website
         userWebsite.href = users.website
 
-        const userRua = document.createElement("h4")
+        userUsuario.append(userName, userUsername, userPhone, userEmail, userWebsite)
+        //----------------RUA--------------------------
+        const userLocalizacao = document.createElement("article")
+
+        const userRua = document.createElement("h3")
         userRua.textContent = 'Rua'
 
-        userElement.append(userName, userUsername, userPhone, userEmail, userWebsite, userRua,document.createElement("hr"))
+        const userEndereco = document.createElement("p")
+        userEndereco.textContent = `Endereço: ${users.address.street}`
+
+        const userCidade = document.createElement("p")
+        userCidade.textContent = `Cidade: ${users.address.city}`
+
+        const userSuit = document.createElement("p")
+        userSuit.textContent = `Suite: ${users.address.suite}`
+
+        const userZipcode = document.createElement("p")
+        userZipcode.textContent = `Zipcode: ${users.address.zipcode}`
+
+        const userGeo = document.createElement("p")
+        userGeo.textContent = `Latencia: ${users.address.geo.lat} \nLongitude: ${users.address.geo.lng}`
+
+        userLocalizacao.append(userRua, userEndereco, userCidade, userSuit, userZipcode, userGeo)
+
+        //---------------------COMPANHIA------------------------------------------
+        const userEmpresa = document.createElement("article")
+
+        const userCompanhia = document.createElement("h3")
+        userCompanhia.textContent = 'Companhia'
+
+        const userCName = document.createElement("p")
+        userCName.textContent = `Nome: ${users.company.name}`
+
+        const userPhrase = document.createElement("p")
+        userPhrase.textContent = `Bordão: ${users.company.catchPhrase}`
+
+        const userBs = document.createElement("p")
+        userBs.textContent = `BS: ${users.company.bs}`
+
+        userEmpresa.append(userCompanhia, userCName, userPhrase, userBs)
+
+        userElement.append(userUsuario, userLocalizacao, userEmpresa,document.createElement("hr"))
         userList.appendChild(userElement)
     })
 }
