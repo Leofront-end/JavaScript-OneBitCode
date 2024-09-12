360ea0234e470e9bdf75cbe94a0054bd14bac0b5
Leo Front-end <ls171258@gmail.com>
2024-09-12 16:12:25 -0300

    Ex2Aula8

--- ex2Aula8/main.js
+++ ex2Aula8/main.js
@@ -48,15 +48,68 @@ async function carregarLivros() {
 function exibirLivros(livros){
     livrosList.innerHTML = ""
     livros.forEach(function (livro){
-        const listarItem = document.createElement("li")
-        listarItem.innerHTML = `<article><h3>${livro.title}</h3><p>Genero: ${livro.genero}</p><p>Pagina atual: ${livro.paginaAtual}</p><p>Total de paginas: ${livro.totalPaginas}</p><buton>oi</button></article><hr>`
-        livrosList.appendChild(listarItem)
+        // const listarItem = document.createElement("li")
+        // listarItem.innerHTML = `<article><h3>${livro.title}</h3><p>Genero: ${livro.genero}</p><p>Pagina atual: ${livro.paginaAtual}</p><p>Total de paginas: ${livro.totalPaginas}</p><buton>oi</button></article><hr>`
+        // livrosList.appendChild(listarItem)
 
-        if (livro.paginaAtual == livro.totalPaginas){
-            listarItem.style.backgroundColor = '#3cf13c54'
+        // if (livro.paginaAtual == livro.totalPaginas){
+        //     listarItem.style.backgroundColor = '#3cf13c54'
+        // }
+
+        const listarLivros = document.createElement("li")
+
+        const titulo = document.createElement("h2")
+        titulo.textContent = livro.title
+
+        const genero = document.createElement("p")
+        genero.textContent = `Genero: ${livro.genero}`
+
+        const paginas = document.createElement("p")
+        paginas.textContent = `Paginas ${livro.paginaAtual}/${livro.totalPaginas}`
+
+        
+
+        
+
+        const deleteButton = document.createElement("button")
+        deleteButton.textContent = `Excluir`
+        deleteButton.addEventListener("click",() => deleteLivro(livro.id))
+
+        
+        listarLivros.append(titulo,genero,paginas)
+
+        if (livro.paginaAtual < livro.totalPaginas){
+            const  editbutton = document.createElement("button")
+            editbutton.textContent = `Atualizar`
+            editbutton.addEventListener("click", () => editBook(livro))
+            listarLivros.append(editbutton)
+        } else {
+            listarLivros.style.backgroundColor = '#6b8e23'
         }
 
-       
+        listarLivros.append(deleteButton)
+        livrosList.appendChild(listarLivros)
+
     })
 }
 
+async function editBook(livro){
+    const paginaAtual = parseInt(prompt("Digite o numero da pagina atual de agora: "))
+    if (isNaN(paginaAtual) || paginaAtual < 0 || paginaAtual > livro.totalPaginas) {
+        return alert("Insira uma quantidade de paginas validas!")
+    }
+
+    const body = JSON.stringify({paginaAtual: parseInt(paginaAtual)})
+
+    await fetch(`http://localhost:3000/Livros/${livro.id}`,{
+        method:"PATCH",
+        headers: {
+            'Content-Type': "apllication/json"
+        },
+        body: body
+    })
+}
+
+async function deleteLivro(id){
+    await fetch(`http://localhost:3000/Livros/${id}`,{ method:"DELETE"})
+}
